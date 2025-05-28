6c95207b6b278843caca4a5421090e332e253d5d
GowthamSivasubramaniam <gowthamsivasubramaniam03@gmail.com>
2025-05-28 11:58:56 +0530

    done repositories

---
+++ 28-may-2025/DoctorAppointment/Models/DoctorSpeciality.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace DoctorAppointment.Models
+{
+    public class DoctorSpeciality
+    {   
+        public int SerialNumber { get; set; }
+        public int DoctorId { get; set; }
+        public int SpecialityId { get; set; }
+       
+        public Speciality? Speciality { get; set; }
+        public Doctor? Doctor { get; set; }
+    }
+}
