af90fda17157f92c9951c99475a35e6442055eb0
GowthamSivasubramaniam <presidio@Presidios-MacBook-Pro.local>
2025-07-09 09:47:01 +0530

    completed tasks

---
+++ 8-july-2025/Azuredbconnect/Program.cs
@@ -0,0 +1,33 @@
+using AzConnect.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers()
+    .AddJsonOptions(opts =>
+    {
+        opts.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+    });
+builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddDbContext<Context>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+}
+);
+
+var app = builder.Build();
+
+
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.MapControllers();
+
+app.Run();
+
